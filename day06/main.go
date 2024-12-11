b95a5f1c5b6c49cd42b91b0aa14849112e979d28
Riemer van der Zee <riemervdzee@gmail.com>
2024-12-11 23:25:01 +0100

    Add day6 part 2

--- day06/main.go
+++ day06/main.go
@@ -5,61 +5,94 @@ import (
 	"riemer/utils"
 )
 
-type Player struct {
+type Guard struct {
 	position  utils.Vector
 	direction utils.Vector
 }
 
 func Process() {
 	total1 := processPart1()
+	total2 := processPart2()
 	fmt.Println("Day 6 Results")
 	fmt.Println("Part1", total1)
+	fmt.Println("Part2", total2)
 }
 
 func processPart1() int {
 	grid := utils.ReadFileAsGrid("day06/input.txt")
-	player := FindPlayer(grid)
+	_, total := simulateGuard(grid, false)
+	return total
+}
+
+func processPart2() int {
+	grid := utils.ReadFileAsGrid("day06/input.txt")
+	guard := findGuard(grid)
+
+	validPositions := 0
+	for y, row := range grid {
+		for x, cell := range row {
+			if cell == '.' && (x != guard.position[0] || y != guard.position[1]) {
+				grid[y][x] = '#'
+				loop, _ := simulateGuard(grid, true)
+				if loop {
+					validPositions++
+				}
+				grid[y][x] = '.'
+			}
+		}
+	}
+	return validPositions
+}
+
+// simulateGuard - Simulates the movements of a guard inside a grid
+func simulateGuard(grid utils.Grid, detectLoops bool) (bool, int) {
+	guard := findGuard(grid)
+	score := 1
+	visited := map[string]bool{}
 
 	for {
-		newPosition := utils.VectorAdd(player.position, player.direction)
+		// Set new position and check if still in bounds of the grid
+		newPosition := utils.VectorAdd(guard.position, guard.direction)
 		if !utils.GridValidPosition(grid, newPosition) {
 			break
 		}
 
-		lookahead := grid[newPosition[1]][newPosition[0]]
-		fmt.Println("Stepping at", newPosition, lookahead)
-
-		if lookahead != '#' {
-			player.position = newPosition
-			grid[newPosition[1]][newPosition[0]] = 'X'
-		} else {
-			fmt.Println("Blocked at", newPosition)
-			player.direction = utils.VectorTurnRight(player.direction)
-		}
-	}
-
-	// Count Distinct places
-	total := 0
-	for _, row := range grid {
-		for _, cell := range row {
-			if cell == 'X' || cell == '^' {
-				total++
+		// Loop detection
+		if detectLoops {
+			guardKey := fmt.Sprintf("%d,%d|%d,%d", newPosition[0], newPosition[1], guard.direction[0], guard.direction[1])
+			if visited[guardKey] {
+				return true, score
 			}
+			visited[guardKey] = true
+		}
+
+		// Progress Guard movements
+		lookahead := grid[newPosition[1]][newPosition[0]]
+		if lookahead != '#' {
+			guard.position = newPosition
+			if !detectLoops && lookahead != 'X' {
+				score++
+				grid[newPosition[1]][newPosition[0]] = 'X'
+			}
+		} else {
+			guard.direction = utils.VectorTurnRight(guard.direction)
 		}
 	}
-	return total
+
+	return false, score
 }
 
-func FindPlayer(grid utils.Grid) Player {
+// findPlayer - Returns a Guard object at the position found in the grid
+func findGuard(grid utils.Grid) Guard {
 	for y, row := range grid {
 		for x, cell := range row {
 			if cell == '^' {
-				return Player{
+				return Guard{
 					position:  utils.Vector{x, y},
 					direction: utils.DirUp,
 				}
 			}
 		}
 	}
-	panic("Player not found")
+	panic("Guard not found")
 }
