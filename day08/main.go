6907d43e953f57f3e0749ab651ac4ad23fdc4b80
Riemer van der Zee <riemervdzee@gmail.com>
2024-12-12 23:29:52 +0100

    Add day08 part 2

--- day08/main.go
+++ day08/main.go
@@ -9,17 +9,25 @@ type NodeMap map[uint8][]utils.Vector
 
 func Process() {
 	grid := utils.ReadFileAsGrid("day08/input.txt")
-	part1Total := 0
 
 	nodeMap := getNodes(grid)
-	antiNodes := getAntiNodes(grid, nodeMap)
-	part1Total = len(antiNodes)
+	part1Total := len(getAntiNodes(grid, nodeMap, false))
+	part2Nodes := getAntiNodes(grid, nodeMap, true)
+
+	// Also mark antenna's in part2Nodes
+	for _, nodes := range nodeMap {
+		for _, node := range nodes {
+			part2Nodes[node] = true
+		}
+	}
+	part2Total := len(part2Nodes)
 
 	fmt.Println("Day 8 Results")
 	fmt.Println("Part1", part1Total)
+	fmt.Println("Part2", part2Total)
 }
 
-func getAntiNodes(grid utils.Grid, nodeMap NodeMap) map[utils.Vector]bool {
+func getAntiNodes(grid utils.Grid, nodeMap NodeMap, repeat bool) map[utils.Vector]bool {
 	antiNodes := make(map[utils.Vector]bool)
 	for _, nodes := range nodeMap {
 		for i := 0; i < len(nodes); i++ {
@@ -30,11 +38,19 @@ func getAntiNodes(grid utils.Grid, nodeMap NodeMap) map[utils.Vector]bool {
 				x1 := utils.VectorSub(v1, diff)
 				x2 := utils.VectorAdd(v2, diff)
 
-				if utils.GridValidPosition(grid, x1) {
+				for utils.GridValidPosition(grid, x1) {
 					antiNodes[x1] = true
+					x1 = utils.VectorSub(x1, diff)
+					if !repeat {
+						break
+					}
 				}
-				if utils.GridValidPosition(grid, x2) {
+				for utils.GridValidPosition(grid, x2) {
 					antiNodes[x2] = true
+					x2 = utils.VectorAdd(x2, diff)
+					if !repeat {
+						break
+					}
 				}
 			}
 		}
