b078116f3cb3bf49171b6e44ffdbeac16f4d9167
Riemer van der Zee <riemervdzee@gmail.com>
2025-01-04 12:34:40 +0100

    Improv day14 speed

--- day14/main.go
+++ day14/main.go
@@ -9,11 +9,13 @@ import (
 
 var re = regexp.MustCompile(`p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)`)
 
-const gridWidth = 101
-const gridHeight = 103
-const secondsPart1 = 100
-const gridWM = gridWidth / 2
-const gridHM = gridHeight / 2
+const (
+	gridWidth    = 101
+	gridHeight   = 103
+	secondsPart1 = 100
+	gridWM       = gridWidth / 2
+	gridHM       = gridHeight / 2
+)
 
 type Robot struct {
 	pos utils.Vector
@@ -34,17 +36,28 @@ func Process() {
 	}
 
 	// Just loop an X amount of times
-	part1 := 0
-	part2 := 0
+	var part1, part2 int
 	safetyFactorP2 := math.MaxInt64
+	quadrants := make([]int, 4)
+
 	for i := 1; i < gridWidth*gridHeight; i++ {
 		// Move all robots and get quadrant scores
-		var quadrants [4]int
+		for i := range quadrants {
+			quadrants[i] = 0
+		}
 		for i := range robots {
-			robots[i].pos = utils.VectorAdd(robots[i].pos, robots[i].vel)
-			robots[i].pos = utils.Vector{modPositive(robots[i].pos[0], gridWidth), modPositive(robots[i].pos[1], gridHeight)}
-			if index, ok := quadIndex(robots[i].pos); ok {
-				quadrants[index] = quadrants[index] + 1
+			robots[i].pos[0] = (robots[i].pos[0] + robots[i].vel[0] + gridWidth) % gridWidth
+			robots[i].pos[1] = (robots[i].pos[1] + robots[i].vel[1] + gridHeight) % gridHeight
+			x, y := robots[i].pos[0], robots[i].pos[1]
+			if x != gridWM && y != gridHM {
+				quadrant := 0
+				if x < gridWM {
+					quadrant++
+				}
+				if y < gridHM {
+					quadrant += 2
+				}
+				quadrants[quadrant]++
 			}
 		}
 
@@ -62,23 +75,4 @@ func Process() {
 	fmt.Println("Day 14 Results")
 	fmt.Println("Part1", part1)
 	fmt.Println("Part2", part2)
-}
-
-// quadIndex returns in which quadrant a position is inside
-func quadIndex(vector utils.Vector) (quad int, found bool) {
-	if vector[0] == gridWM || vector[1] == gridHM {
-		return quad, false
-	}
-	if vector[0] < gridWM {
-		quad++
-	}
-	if vector[1] < gridHM {
-		quad += 2
-	}
-	return quad, true
-}
-
-// mod is a modulo with a positive remainder
-func modPositive(a, b int) int {
-	return (a%b + b) % b
 }
